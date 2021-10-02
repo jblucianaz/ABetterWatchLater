9a093ed802f5d1cd204e66d4181cdc1ef7d14955
Jean-Baptiste Lucianaz <jb.lucianaz@gmail.com>
2021-10-02 14:50:57 +0200

    Create constructor for the YouTubeChannel class.

--- API/ABetterWatchLaterAPI/Models/YouTubeChannel.cs
+++ API/ABetterWatchLaterAPI/Models/YouTubeChannel.cs
@@ -4,10 +4,17 @@ namespace ABetterWatchLaterAPI
 {
     public class YouTubeChannel
     {
-        public string Id { get; set; }
+        public string ChannelId { get; set; }
 
         public string Title { get; set; }
 
-        public string thumbnail { get; set; }
+        public string Thumbnail { get; set; }
+
+        public YouTubeChannel(string channelId, string title, string thumbnail)
+        {
+            ChannelId = channelId;
+            Title = title;
+            Thumbnail = thumbnail;
+        }
     }
 }
